c2cf89247c276ba54ac7370aa31d0a99c90a4118
malhayekk <134982942+malhayekk@users.noreply.github.com>
2025-02-17 21:29:02 -0500

    now able to get color schemes!

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const formEl = document.querySelector("form")
 const seedColorEl = document.getElementById("seedColor")
 const colorSchemeModeEl = document.getElementById("colorSchemeMode")
+let colorSection = 1
 
 formEl.addEventListener("submit", function (e) {
     e.preventDefault()
     getColorScheme(seedColorEl.value, colorSchemeModeEl.value)
+    formEl.reset()
 })
 
 
@@ -13,11 +15,23 @@ function getColorScheme(hex, mode) {
     console.log(`hex is ${hex} and scheme is ${mode}`)
     const baseURL = "https://www.thecolorapi.com"
     const endPoint = `/scheme?hex=${hex}&mode=${mode}`
-    const path = baseURL+endPoint
+    const path = baseURL + endPoint
     console.log(`fetch path = ${path}`)
     fetch(`${path}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            const colors = data.colors
+            colors.forEach(displayColor)
         });
 }
+
+function displayColor(color) {
+    console.log(color.hex.value)
+    const colorHexValue = color.hex.value
+    const sectionToColorEl = document.getElementById(`color${colorSection}`)
+    sectionToColorEl.style.backgroundColor = color.hex.value
+    colorSection++
+}
+
+/*0 - > hex: {value: '#AC1F20', clean: 'AC1F20'}
+*/
